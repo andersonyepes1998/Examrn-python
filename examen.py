29e4d13b2a3924862e90772058fa004a6740e3bb
anderson <andersonyepesbedoya@gmail.com>
2023-03-17 23:04:06 -0500

    nuevos cambios

--- examen.py
+++ examen.py
@@ -1,4 +1,30 @@
-items = 5
-while items < 15:
-    items= items+2
-    print(items)
+
+numeros =[(50,45,20,30,40,87)]
+
+print(numeros)
+
+conjunto = set().union(*numeros)
+print(conjunto)
+
+resultado = sorted(conjunto)
+print(resultado)
+
+#--------------------------------
+
+tuplas = (50,45,20,30,40,87)
+
+listas = list(tuplas)
+for lista in listas:
+    if lista>40:
+        print(lista)
+
+#--------------------------------
+
+
+tuplas2 = (50,45,20,30,40,87)
+
+numerolistas = list(tuplas2)
+for i in numerolistas:
+    if i/2:
+        print(i)
+
